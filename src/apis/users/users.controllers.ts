9060da64a5d7d887e74112e2bf63074b36cb2de7
Ali Abdullah <aliyabdulah@gmail.com>
2025-06-24 20:35:41 +0300

    First commit

--- src/apis/users/users.controllers.ts
+++ src/apis/users/users.controllers.ts
@@ -1,27 +1,62 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
-    } catch (err) {
-        next(err);
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if username already exists
+    const existingUserName = await User.findOne({ username });
+    if (existingUserName) {
+      res.status(400).json({ message: "User already in use" });
     }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create and save the user
+    const user = new User({ username, password: hashedPassword });
+
+    // Generate token
+    const token = jwt.sign(
+      { userId: user._id, email: user.username },
+      process.env.JWT_SK as string,
+      { expiresIn: "1h" }
+    );
+    await user.save();
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
-export const signin = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-    } catch (err) {
-        next(err);
-    }
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await User.find().populate("urls");
-        res.status(201).json(users);
-    } catch (err) {
-        next(err);
-    }
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find().populate("urls");
+    res.status(201).json(users);
+  } catch (err) {
+    next(err);
+  }
 };
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import usersRouter from "./apis/users/users.routes";
 import urlsRouter from "./apis/urls/urls.routes";
 
 const app = express();
-const PORT = 8000;
+const PORT = 5000;
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -20,5 +20,5 @@ app.use(errorHandler);
 
 connectDB();
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
